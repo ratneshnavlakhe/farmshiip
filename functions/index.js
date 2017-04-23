d1b239b39a9354db34e0588de4063111fc485c98
Ratnesh <ratnesh061288@gmail.com>
2017-04-23 10:11:47 +0530

    Added a addRobot api, that lookups the qrcode and find the robo after scanning

--- functions/index.js
+++ functions/index.js
@@ -9,37 +9,23 @@ var functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
-exports.findRobot = functions.database.ref('/robot')
-  .onWrite(event => {
-    const original = event.data.val();
-    console.log('original', original);
-    return;
-});
-
 exports.addRobot = functions.https.onRequest((req, res) => {
   // Grab the text parameter.
-  const original = req.query.text;
-  // Push it into the Realtime Database then send a response
-  // admin.database().ref('/messages').push({original: original}).then(snapshot => {
-  //   // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
-  //   res.redirect(303, snapshot.ref);
-  // });
-  admin.database().ref('/robot').then(snapshot => {
-    console.log(snapshot);
-    res.redirect(303, snapshot.ref);
+  const qrcode = req.query.qrcode;
+  admin.database().ref('/robot').once('value').then(snapshot => {
+  const data = snapshot.val();
+  var robot;
+    data.map(function(x) {
+      if (x.robot_id == qrcode) {
+        robot = x.robot_id;
+      }
+    });
+    console.log('logged robot '+robot);
+    admin.database().ref('/controller/'+robot).set({
+      connection_status: true,
+      direction: 'x',
+      spray: false
+    });
+    res.send("Hello from Firebase!");
   });
 });
-
-
-exports.makeUppercase = functions.database.ref('/object')
-    .onWrite(event => {
-      // Grab the current value of what was written to the Realtime Database.
-      const original = event.data.val();
-      console.log('Uppercasing', event.params.pushId, original);
-      // const uppercase = original.toUpperCase();
-      // You must return a Promise when performing asynchronous tasks inside a Functions such as
-      // writing to the Firebase Realtime Database.
-      // Setting an "uppercase" sibling in the Realtime Database returns a Promise.
-      // return event.data.ref.parent.child('uppercase').set(uppercase);
-      return;
-    });
